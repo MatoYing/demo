9c53fd9840d032046b396594f0810daeaea6ee53
Mato <1350478903@qq.com>
2022-11-15 23:18:00 +0800

    second

---
+++ src/demo1.java
@@ -0,0 +1,11 @@
+/**
+ * @Description
+ * @Author
+ * @Date 12/11/2022 11:42 PM
+ */
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
